c2e3a821d8a71d595185b05355d56a9c85fad068
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-10 16:34:05 -0400

    Feat: add gnral configs

--- examples/basic-server/index.js
+++ examples/basic-server/index.js
@@ -6,9 +6,7 @@ const chance = new Chance();
 // Define a server
 const server = http.createServer(function (request, response) {
   response.writeHead(200, { "content-type": "text/html" }); //Header of the content
-  response.end(
-    `Wake up, <b>${chance.animal()}</b>... follow the white rabbit `
-  ); // content
+  response.end(`Wake up, <b>${chance.name()}</b>... follow the white rabbit `); // content
 });
 
 // Start the server
