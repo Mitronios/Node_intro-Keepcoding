16d217ccb1973225bb019d6d00ec5e2649d3f31b
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-11 14:45:39 -0400

    Feat: add callback loop"

--- examples/asynchronous.js
+++ examples/asynchronous.js
@@ -7,8 +7,28 @@ function logAfterTwoSeconds(text, callback) {
   }, 2000);
 }
 
+/*
 logAfterTwoSeconds("Hello", function () {
   logAfterTwoSeconds("Hello Again", function () {
     console.log("The end");
   });
+});
+*/
+//Here we are using callbacks to make the function asynchronous
+
+//callbacks loop maker
+function serial(n, funcWeWantToExecute, lastCallBack) {
+  if (n === 0) {
+    //End here
+    lastCallBack();
+    return;
+  }
+  n = n - 1;
+  funcWeWantToExecute("Hello" + n, function () {
+    serial(n, funcWeWantToExecute, lastCallBack);
+  });
+}
+
+serial(5, logAfterTwoSeconds, function () {
+  console.log("The End");
 });
