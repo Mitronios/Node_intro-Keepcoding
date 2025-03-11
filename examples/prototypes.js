7836629ee25346773e6adfac843affe7d29e6d4e
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-11 15:48:39 -0400

    Feat: add protopypes

--- examples/prototypes.js
+++ examples/prototypes.js
@@ -19,3 +19,38 @@ Person.prototype.greet = function () {
 
 peter.greet();
 mary.greet();
+
+//Simple inheritance
+//Let's create Agents from Matrix
+
+function Agent(name) {
+  //inherit constructor from Person
+  Person.call(this, name); //Similar to super() in Python
+}
+
+Object.setPrototypeOf(Agent.prototype, Person.prototype);
+
+const smith = new Agent("Smith");
+
+smith.greet();
+
+//Multiple inheritance
+
+//Now we want agents to inherit 'superpowers'
+
+function SuperPower() {
+  this.fly = function () {
+    console.log(this.name, "Fly");
+  };
+}
+
+//copying all properties from superpowers
+Object.assign(Agent.prototype, new SuperPower());
+
+smith.fly();
+
+console.log(smith);
+console.log(Agent.prototype);
+console.log(smith instanceof Person);
+console.log(smith instanceof Agent);
+console.log(smith instanceof SuperPower);
