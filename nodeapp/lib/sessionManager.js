520963f579a9512ffe93e14a7b39a68522ba87dd
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-21 17:52:36 -0400

    Fix: change secret and add secret to .env, add connect-mongo

--- nodeapp/lib/sessionManager.js
+++ nodeapp/lib/sessionManager.js
@@ -1,14 +1,20 @@
 import session from "express-session";
+import MongoStore from "connect-mongo";
+import dotenv from "dotenv";
 
 const INACTIVITY_EXPIRATION_2_DAYS = 1000 * 60 * 60 * 24 * 2; // 48 hours
-
+const password = process.env.MONGODB_PASSWORD;
+const secretSession = process.env.SESSION_SECRET;
 //middleware for the sessions
 export const middleware = session({
   name: "nodeapp-session",
-  secret: "xthaM7yJ8L6R7UWTK19SsBRjkEkYmr",
+  secret: `${secretSession}`,
   saveUninitialized: true, //Creates an empty session for every user, logged or not
   resave: false, //this and above are not absolutely necessary
   cookie: { maxAge: INACTIVITY_EXPIRATION_2_DAYS },
+  store: MongoStore.create({
+    mongoUrl: `mongodb://admin:${password}@localhost:27017/cursonode?authSource=admin`,
+  }),
 });
 
 export function useSessionsInViews(req, res, next) {
